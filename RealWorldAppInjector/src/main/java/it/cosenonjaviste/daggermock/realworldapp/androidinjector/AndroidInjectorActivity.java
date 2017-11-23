1f8c65f7faabf15e31a73631596bae727107e4c7
Thomas Schmidt <bohsen@gmail.com>
2017-11-23 14:26:45 +0100

    Added extra Activity to show how ContributesAndroidInjector workes and how it's tested with DaggerMock

---
+++ RealWorldAppInjector/src/main/java/it/cosenonjaviste/daggermock/realworldapp/androidinjector/AndroidInjectorActivity.java
@@ -0,0 +1,56 @@
+/*
+ *   Copyright 2016 Fabio Collini.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package it.cosenonjaviste.daggermock.realworldapp.androidinjector;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import it.cosenonjaviste.daggeroverride.R;
+
+public class AndroidInjectorActivity extends AppCompatActivity implements AndroidInjectorView {
+
+    @Inject
+    AndroidInjectorPresenter presenter;
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        textView = (TextView) findViewById(R.id.text);
+
+        findViewById(R.id.reload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.loadData();
+            }
+        });
+    }
+
+    @Override
+    public void showText(String text) {
+        textView.setText(text);
+    }
+}
